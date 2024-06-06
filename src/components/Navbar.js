705f6db4a0dc5c344ce1bbb06d289b4385d341c9
mrams07 <amansakdasariya@gmail.com>
2024-06-06 17:59:02 +0530

    after Next btn and Previous btn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
     render() {
         return (
-            <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className='navCont'>
+                <nav className="navbar navbar-expand-lg bg-primary navbar-primary">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/"><b>NewsBaba</b></a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
